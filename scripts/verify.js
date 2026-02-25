f5be19aa5e495a93c16f27b9e25dda1965777528
WookiesRpeople2 <keentobiansky@gmail.com>
2026-02-25 12:06:40 +0100

    chore: update release

--- scripts/verify.js
+++ scripts/verify.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const config = require("../jest.config.js");
 
-const file = path.resolve(__dirname, process.env.DIR);
+const file = path.resolve(process.cwd(), process.env.DIR);
 
 if (!fs.existsSync(file)) {
   console.error('Coverage file not found');
